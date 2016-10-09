e2d09b3c30171c827822c36c526f80aa54550244
Matthew Sacchetti <sacchetti.1@osu.edu>
2016-10-09 15:01:11 -0400

    Add files via upload
    This is a fun little program using information about myself.

---
+++ mebackground.py
@@ -0,0 +1,20 @@
+me = {'name': 'Matthew', 'age': 37, 'height': 'Six Feet Two Inches', 'location':
+'United States of America'}
+
+print "What is your first name?"
+print me['name']
+
+print '-' * 10
+print "What age are you?"
+print me['age']
+
+print '-' * 10
+print "What is your current height?"
+print me['height']
+
+print '-' * 10
+print "Where do you reside?"
+print me['location']
+
+print '-' * 10
+print me
